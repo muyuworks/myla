cbbdb4241b12a33178d83fd0292b88b3d5ac7ea9
shellc <wangshenggong@gmail.com>
2023-11-13 17:06:50 +0800

    FIX: apis to async

--- myla/_api.py
+++ myla/_api.py
@@ -48,13 +48,13 @@ async def get_version():
 
 
 @api.post("/v1/assistants", response_model=assistants.AssistantRead, tags=['Assistants'])
-def create_assistant(assistant: assistants.AssistantCreate):
+async def create_assistant(assistant: assistants.AssistantCreate):
     r = assistants.create(assistant=assistant)
     return r
 
 
 @api.get("/v1/assistants/{assistant_id}", response_model=assistants.AssistantRead, tags=['Assistants'])
-def retrieve_assistant(assistant_id: str):
+async def retrieve_assistant(assistant_id: str):
     a = assistants.get(id=assistant_id)
     if not a:
         raise HTTPException(status_code=404, detail="Thread not found")
@@ -62,30 +62,30 @@ def retrieve_assistant(assistant_id: str):
 
 
 @api.post("/v1/assistants/{assistant_id}", response_model=assistants.AssistantRead, tags=['Assistants'])
-def modify_assistant(assistant_id: str, assistant: assistants.AssistantModify):
+async def modify_assistant(assistant_id: str, assistant: assistants.AssistantModify):
     return assistants.modify(id=assistant_id, assistant=assistant)
 
 
 @api.delete("/v1/assistants/{assistant_id}", tags=['Assistants'])
-def delete_assistant(assistant_id: str):
+async def delete_assistant(assistant_id: str):
     return assistants.delete(id=assistant_id)
 
 
 @api.get("/v1/assistants", response_model=ListModel, tags=['Assistants'])
-def list_assistants(limit: int = 20, order: str = "desc", after: str = None, before: str = None):
+async def list_assistants(limit: int = 20, order: str = "desc", after: str = None, before: str = None):
     return assistants.list(limit=limit, order=order, after=after, before=before)
 
 # Threads
 
 
 @api.post("/v1/threads", response_model=threads.ThreadRead, tags=['Threads'])
-def create_thread(thread: threads.ThreadCreate):
+async def create_thread(thread: threads.ThreadCreate):
     r = threads.create(thread=thread)
     return r
 
 
 @api.get("/v1/threads/{thread_id}", response_model=threads.ThreadRead, tags=['Threads'])
-def retrieve_thread(thread_id: str):
+async def retrieve_thread(thread_id: str):
     t = threads.get(id=thread_id)
     if not t:
         raise HTTPException(status_code=404, detail="Thread not found")
@@ -93,30 +93,30 @@ def retrieve_thread(thread_id: str):
 
 
 @api.post("/v1/threads/{thread_id}", response_model=threads.ThreadRead, tags=['Threads'])
-def modify_thread(thread_id: str, thread: threads.ThreadModify):
+async def modify_thread(thread_id: str, thread: threads.ThreadModify):
     return threads.modify(id=thread_id, thread=thread)
 
 
 @api.delete("/v1/threads/{thread_id}", tags=['Threads'])
-def delete_thread(thread_id: str):
+async def delete_thread(thread_id: str):
     return threads.delete(id=thread_id)
 
 
 @api.get("/v1/threads", response_model=ListModel, tags=['Threads'])
-def list_threads(limit: int = 20, order: str = "desc", after: str = None, before: str = None):
+async def list_threads(limit: int = 20, order: str = "desc", after: str = None, before: str = None):
     return threads.list(limit=limit, order=order, after=after, before=before)
 
 # Messages
 
 
 @api.post("/v1/threads/{thread_id}/messages", response_model=messages.MessageRead, tags=['Messages'])
-def create_message(thread_id: str, message: messages.MessageCreate):
+async def create_message(thread_id: str, message: messages.MessageCreate):
     r = messages.create(thread_id=thread_id, message=message)
     return r
 
 
 @api.get("/v1/threads/{thread_id}/messages/{message_id}", response_model=messages.MessageRead, tags=['Messages'])
-def retrieve_message(thread_id: str, message_id: str):
+async def retrieve_message(thread_id: str, message_id: str):
     t = messages.get(id=message_id)
     if not t:
         raise HTTPException(status_code=404, detail="Thread not found")
@@ -124,30 +124,30 @@ def retrieve_message(thread_id: str, message_id: str):
 
 
 @api.post("/v1/threads/{thread_id}/messages/{message_id}", response_model=messages.MessageRead, tags=['Messages'])
-def modify_message(thread_id: str, message_id: str, message: messages.MessageModify):
+async def modify_message(thread_id: str, message_id: str, message: messages.MessageModify):
     return messages.modify(id=message_id, message=message)
 
 
 @api.delete("/v1/threads/{thread_id}/messages/{message_id}", tags=['Messages'])
-def delete_message(thread_id: str, message_id: str):
+async def delete_message(thread_id: str, message_id: str):
     return messages.delete(id=message_id)
 
 
 @api.get("/v1/threads/{thread_id}/messages", response_model=messages.MessageList, tags=['Messages'])
-def list_messages(thread_id: str, limit: int = 20, order: str = "desc", after: str = None, before: str = None):
+async def list_messages(thread_id: str, limit: int = 20, order: str = "desc", after: str = None, before: str = None):
     return messages.list(thread_id=thread_id, limit=limit, order=order, after=after, before=before)
 
 # Runs
 
 
 @api.post("/v1/threads/{thread_id}/runs", response_model=runs.RunRead, tags=['Runs'])
-def create_run(thread_id: str, run: runs.RunCreate):
+async def create_run(thread_id: str, run: runs.RunCreate):
     r = runs.create(thread_id=thread_id, run=run)
     return r
 
 
 @api.get("/v1/threads/{thread_id}/runs/{run_id}", response_model=runs.RunRead, tags=['Runs'])
-def retrieve_run(thread_id: str, run_id: str):
+async def retrieve_run(thread_id: str, run_id: str):
     t = runs.get(thread_id=thread_id, run_id=run_id)
     if not t:
         raise HTTPException(status_code=404, detail="Run not found")
@@ -155,33 +155,33 @@ def retrieve_run(thread_id: str, run_id: str):
 
 
 @api.post("/v1/threads/{thread_id}/runs/{run_id}", response_model=runs.RunRead, tags=['Runs'])
-def modify_run(thread_id: str, run_id: str, run: runs.RunModify):
+async def modify_run(thread_id: str, run_id: str, run: runs.RunModify):
     return runs.modify(id=run_id, run=run)
 
 
 @api.delete("/v1/threads/{thread_id}/runs/{run_id}", tags=['Runs'])
-def delete_message(thread_id: str, run_id: str):
+async def delete_message(thread_id: str, run_id: str):
     return runs.delete(id=run_id)
 
 
 @api.get("/v1/threads/{thread_id}/runs", response_model=ListModel, tags=['Runs'])
-def list_runs(thread_id: str, limit: int = 20, order: str = "desc", after: str = None, before: str = None):
+async def list_runs(thread_id: str, limit: int = 20, order: str = "desc", after: str = None, before: str = None):
     return runs.list(thread_id=thread_id, limit=limit, order=order, after=after, before=before)
 
 @api.post("/v1/threads/{thread_id}/runs/{run_id}/cancel", response_model=runs.RunRead, tags=['Runs'])
-def cancel_run(thread_id: str, run_id: str):
+async def cancel_run(thread_id: str, run_id: str):
     return runs.cancel(thread_id=thread_id, run_id=run_id)
 
 @api.post("/v1/threads/runs", response_model=runs.RunRead, tags=['Runs'])
-def create_thread_and_run(thread_run: runs.ThreadRunCreate):
+async def create_thread_and_run(thread_run: runs.ThreadRunCreate):
     return runs.create_thread_and_run(thread_run=thread_run)
 
 @api.get("/v1/threads/{thread_id}/runs/{run_id}/steps/{step_id}", response_model=runs.RunStep, tags=['Runs'])
-def retrieve_run_step(thread_id: str, run_id: str, step_id: str):
+async def retrieve_run_step(thread_id: str, run_id: str, step_id: str):
     return runs.get_step(thread_id=thread_id, run_id=run_id, step_id=step_id)
 
 @api.get("/v1/threads/{thread_id}/runs/{run_id}/steps", response_model=ListModel, tags=['Runs'])
-def list_run_steps(thread_id: str, run_id: str):
+async def list_run_steps(thread_id: str, run_id: str):
     return runs.list_steps(thread_id=thread_id, run_id=run_id)
 
 @api.get("/v1/threads/{thread_id}/runs/{run_id}/stream", tags=['Runs'])
