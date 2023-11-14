5594addba1f2e275011dac4538609f0a023b44bd
shellc <wangshenggong@gmail.com>
2023-11-14 21:49:58 +0800

    FIX: change dict size while iterating

--- myla/_run_queue.py
+++ myla/_run_queue.py
@@ -5,20 +5,20 @@ class AsyncIterator:
     def __init__(self):
         self.queue = asyncio.Queue()
         self.created_at = datetime.now().timestamp()
- 
+
     def __aiter__(self):
         return self
- 
+
     async def __anext__(self):
         i = await self.queue.get()
         if i is None:
             raise StopAsyncIteration
-        
+
         return i
-    
+
     async def put(self, item):
         await self.queue.put(item)
-    
+
 _run_tasks = asyncio.Queue()
 _run_iters = {}
 _lock = asyncio.Lock()
@@ -41,10 +41,13 @@ async def get_run_iter(run_id):
 _last_clear_at = datetime.now().timestamp()
 async def clear_iters():
     now = datetime.now().timestamp()
-    if _last_clear_at + 10*60 > now:
+    if _last_clear_at + 20 > now:
         return
-    
-    for run_id, iter in _run_iters.items():
-        if iter.created_at + 10*60 < now:
-            async with _lock:
-                _run_iters.pop(run_id)
+    async with _lock:
+        expired = []
+        for run_id, iter in _run_iters.items():
+            if iter.created_at + 20 < now:
+                expired.append(run_id)
+
+        for run_id in expired:
+            _run_iters.pop(run_id)
