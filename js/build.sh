7e7af65d845f5cdfe57aec35458b155d768a1b8e
shellc <wangshenggong@gmail.com>
2023-11-15 14:28:39 +0800

    add build scripts

--- js/build.sh
+++ js/build.sh
@@ -1,4 +1,5 @@
 npm run build
 
 mkdir -p ../webui/static/aify
-cp build/* ../webui/static/aify
+cp build/static/js/*.js ../webui/static/aify/aify.js
+cp build/static/css/*.css ../webui/static/aify/aify.css
