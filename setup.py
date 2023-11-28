4cccd2d7e1546516cf99a0875456131eccfe607b
shellc <wangshenggong@gmail.com>
2023-11-28 20:14:57 +0800

    TEST: add unittests

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
     keywords=["AI assistant", "LLM", "Myla", "chatbot"],
     long_description_content_type="text/markdown",
     long_description=read("README.md"),
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests']),
     include_package_data=True,
     package_data={
         '': ['webui/**']
