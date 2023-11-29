f49464ffdfae818a6085c1c2f4be8a73ec88351a
shellc <wangshenggong@gmail.com>
2023-11-29 09:04:47 +0800

    PEP8: add flake8

--- setup.py
+++ setup.py
@@ -13,16 +13,19 @@ from setuptools import setup, find_packages
 here = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(here)
 
+
 def read(*parts):
     with codecs.open(os.path.join(here, *parts), "r") as fp:
         return fp.read()
-    
+
+
 def find_version(*file_paths):
     version_file = read(*file_paths)
     version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
     if version_match:
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
+
 
 version = find_version('myla', '__init__.py')
 
@@ -73,7 +76,7 @@ setup(
     package_data={
         '': ['webui/**']
     },
-    install_requires = install_requires,
+    install_requires=install_requires,
     extras_require=extras_require,
     scripts=['./scripts/myla'],
 )
