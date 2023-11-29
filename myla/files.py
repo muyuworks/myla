4736745197ca13e1656196c5b29f1a70b7d37fcd
shellc <wangshenggong@gmail.com>
2023-11-29 09:04:19 +0800

    PEP8: files.py

--- myla/files.py
+++ myla/files.py
@@ -3,23 +3,28 @@ from sqlmodel import Field, Session, select
 from ._models import auto_session, MetadataModel, DBModel, ReadModel, ListModel, DeletionStatus
 from ._models import create as create_model
 
+
 class FileUpload(MetadataModel):
     purpose: str
+
 
 class FileRead(ReadModel):
     bytes: int
     filename: str
     purpose: str
 
+
 class FileList(ListModel):
     data: List[FileRead]
+
 
 class File(DBModel, table=True):
     bytes: int
     filename: Optional[str] = None
     purpose: str = Field(index=True)
 
-def create(id: str, file: FileUpload, bytes:int, filename:str, session: Optional[Session] = None) -> FileRead:
+
+def create(id: str, file: FileUpload, bytes: int, filename: str, session: Optional[Session] = None) -> FileRead:
     db_model = File(
         purpose=file.purpose,
         bytes=bytes,
@@ -27,15 +32,25 @@ def create(id: str, file: FileUpload, bytes:int, filename:str, session: Optional
         metadata_=file.metadata
     )
 
-    dbo = create_model(id=id, object="file", meta_model=file, db_model=db_model, session=session)
+    dbo = create_model(id=id, object="file", meta_model=file,
+                       db_model=db_model, session=session)
 
     r = FileRead(**dbo.dict())
     r.metadata = dbo.metadata_
 
     return r
 
+
 @auto_session
 def get(id: str, session: Optional[Session] = None) -> Union[FileRead, None]:
+    """Retrieval File object.
+
+    Args:
+        id(str): file id
+
+    Returns:
+        if file exists return FileRead object else return None
+    """
     r = None
     dbo = session.get(File, id)
     if dbo:
@@ -43,6 +58,7 @@ def get(id: str, session: Optional[Session] = None) -> Union[FileRead, None]:
         r = FileRead(**d)
         r.metadata = dbo.metadata_
     return r
+
 
 @auto_session
 def delete(id: str, session: Optional[Session] = None) -> DeletionStatus:
@@ -52,8 +68,9 @@ def delete(id: str, session: Optional[Session] = None) -> DeletionStatus:
         session.commit()
     return DeletionStatus(id=id, object="file.deleted", deleted=True)
 
+
 @auto_session
-def list(purpose:str = None, limit: int = 20, order:str = "desc", after:str = None, before:str = None, session: Optional[Session] = None) -> FileList:
+def list(purpose: str = None, limit: int = 20, order: str = "desc", after: str = None, before: str = None, session: Optional[Session] = None) -> FileList:
     select_stmt = select(File)
 
     if purpose:
@@ -66,7 +83,7 @@ def list(purpose:str = None, limit: int = 20, order:str = "desc", after:str = No
         select_stmt = select_stmt.filter(File.id < before)
 
     select_stmt = select_stmt.limit(limit)
-    
+
     dbos = session.exec(select_stmt).all()
     rs = []
     for dbo in dbos:
