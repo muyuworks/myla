954d74eba12b7c455e0772ecdd4ba7a3b0df8f46
shellc <wangshenggong@gmail.com>
2023-12-01 16:47:56 +0800

    FEATURE: add users

---
+++ myla/users.py
@@ -0,0 +1,181 @@
+from typing import Optional, List
+from pydantic import BaseModel
+from sqlmodel import SQLModel, Field, Session, select
+from . import _models
+from . import utils
+
+class UserOrgLink(SQLModel, table=True):
+    org_id: str = Field(foreign_key="organization.id", primary_key=True)
+    user_id: str = Field(foreign_key="user.id", primary_key=True)
+
+
+class OrganizationBase(BaseModel):
+    """Represents the basic information of an organization."""
+    display_name: Optional[str]
+
+
+class OrganizationCreate(_models.MetadataModel, OrganizationBase):
+    """Represents the Organization to be created."""
+
+
+class OrganizationRead(_models.ReadModel, OrganizationBase):
+    """Represents the Organization read."""
+    is_primary: bool
+
+
+class OrganizationList(_models.ListModel):
+    """Represents the Organization list."""
+    data: List[OrganizationRead] = []
+
+
+class Organization(_models.DBModel, OrganizationBase, table=True):
+    """Represents the user saved in db."""
+    is_primary: bool = Field(index=True, default=False)
+    #users: List["User"] = Relationship(back_populates="orgs", link_model=UserOrgLink)
+
+
+class UserBase(BaseModel):
+    """Represents the basic information of a user."""
+    username: str = Field(index=True, unique=True)
+    display_name: Optional[str]
+
+
+class UserCreate(_models.MetadataModel, UserBase):
+    """Represents the user to be created."""
+    password: str
+
+
+class UserRead(_models.ReadModel, UserBase):
+    """Represents the user read."""
+    orgs: List[Organization] = []
+
+
+class UserList(_models.ListModel):
+    """Represents the user list."""
+    data: List[UserRead] = []
+
+
+class User(_models.DBModel, UserBase, table=True):
+    """Represents the user saved in db."""
+    is_sa: Optional[bool] = Field(default=False)
+    salt: str
+    password: str
+    #orgs: List[Organization] = Relationship(back_populates="users", link_model=UserOrgLink)
+
+
+class SecretKeyBase(BaseModel):
+    secret_key: str = Field(primary_key=True)
+    user_id: str = Field(index=True)
+    display_name: Optional[str]
+
+
+class SecrectKeyCreate(SecretKeyBase):
+    """Secrect object to be created."""
+
+
+class SecrectKeyRead(SecretKeyBase):
+    """The SecrectKey read."""
+
+
+class SecrectKeyList(_models.ReadModel):
+    data: List[SecrectKeyRead] = []
+
+
+class SecretKey(SQLModel, SecretKeyBase, table=True):
+
+    created_at: int = Field(index=True)
+
+
+def create_organization(org: OrganizationCreate, is_primary: bool = False, user_id: str = None, session: Optional[Session] = None, auto_commit=True) -> OrganizationRead:
+    db_model = Organization.from_orm(org)
+    db_model.is_primary = is_primary
+    db_model.user_id = user_id
+
+    dbo = _models.create(object="organization", meta_model=org, db_model=db_model, session=session, auto_commit=auto_commit)
+    r = OrganizationRead(**dbo.dict())
+    r.metadata = dbo.metadata_
+    return r
+
+
+@_models.auto_session
+def get_organization(id: str, session: Optional[Session] = None):
+    r = None
+    dbo = session.get(Organization, id)
+    if dbo:
+        r = OrganizationRead(**dbo.dict())
+        r.metadata = dbo.metadata_
+
+    return r
+
+
+def generate_password(password: str, salt: str):
+    s = f"{password} {salt}"
+    return utils.sha256(s=s)
+
+
+@_models.auto_session
+def create_user(user: UserCreate, is_sa: bool = False, session: Session = None) -> UserRead:
+    salt = utils.uuid()
+    password = generate_password(user.password, salt)
+
+    db_model = User(salt=salt, is_sa=is_sa, **user.dict())
+    db_model.password = password
+
+    if not user.display_name:
+        db_model.display_name = user.username
+
+    user_created = _models.create(object="user", meta_model=user, db_model=db_model, session=session, auto_commit=False)
+
+    org = OrganizationCreate(display_name=user.username)
+    org_created = create_organization(org=org, is_primary=True, user_id=user_created.id, session=session, auto_commit=False)
+
+    link = UserOrgLink(org_id=org_created.id, user_id=user_created.id)
+    session.add(link)
+
+    r = UserRead(**user_created.dict())
+    r.metadata = user_created.metadata_
+
+    session.commit()
+    return r
+
+
+@_models.auto_session
+def get_user_dbo(id: str, session: Session = None):
+    return session.get(User, id)
+
+
+@_models.auto_session
+def get_user(id: str, session: Session = None):
+    r = None
+    dbo = get_user_dbo(id=id, session=session)
+    if dbo:
+        r = UserRead(**dbo.dict())
+        r.metadata = dbo.metadata_
+
+    return r
+
+
+@_models.auto_session
+def list_orgs(user_id: str, session: Session = None):
+    stmt = select(Organization, UserOrgLink).where(Organization.id == UserOrgLink.org_id).filter(UserOrgLink.user_id == user_id)
+    dbos = session.exec(statement=stmt).all()
+
+    rs = []
+    for dbo in dbos:
+        a = OrganizationRead(**dbo[0].dict())
+        rs.append(a)
+    r = OrganizationList(data=rs)
+    return r
+
+
+@_models.auto_session
+def list_sa_users(session: Session = None) -> UserList:
+    stmt = select(User).filter(User.is_sa == True)
+    dbos = session.exec(statement=stmt).all()
+
+    rs = []
+    for dbo in dbos:
+        a = UserRead(**dbo.dict())
+        rs.append(a)
+    r = UserList(data=rs)
+    return r
---
+++ tests/myla/users_test.py
@@ -0,0 +1,60 @@
+import unittest
+
+from myla import users, persistence
+
+
+class TestUsers(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.db = persistence.Persistence(database_url="sqlite://")
+        self.db.initialize_database()
+        self.session = self.db.create_session()
+
+    def tearDown(self) -> None:
+        self.session.close()
+
+    def test_list_sa_users(self):
+        sa_users = users.list_sa_users(self.session)
+        self.assertIsInstance(sa_users, users.UserList)
+        self.assertCountEqual(sa_users.data, [])
+
+    def test_create_organization(self):
+        org = users.create_organization(users.OrganizationCreate(), session=self.session)
+        self.assertIsInstance(org, users.OrganizationRead)
+        self.assertIsNotNone(org.id)
+
+        org_read = users.get_organization(id=org.id, session=self.session)
+        self.assertEqual(org_read.id, org.id)
+        self.assertFalse(org_read.is_primary)
+
+    def test_create_organization_with_metadata(self):
+        org = users.create_organization(users.OrganizationCreate(display_name='org', metadata={'k': 'v'}), session=self.session)
+        self.assertIsInstance(org, users.OrganizationRead)
+        self.assertEqual(org.display_name, 'org')
+        self.assertEqual(org.metadata, {'k': 'v'})
+
+    def test_create_organization_without_auto_commit(self):
+        org = users.create_organization(users.OrganizationCreate(), session=self.session, auto_commit=False)
+        self.assertIsInstance(org, users.OrganizationRead)
+        self.assertIsNotNone(org.id)
+
+        org_read = users.get_organization(id=org.id, session=self.db.create_session())
+        self.assertIsNone(org_read)
+
+    def test_create_user(self):
+        user = users.create_user(user=users.UserCreate(username='shellc', password='shellc'), session=self.session)
+
+        self.assertIsInstance(user, users.UserRead)
+
+        user_read = users.get_user(id=user.id, session=self.session)
+        self.assertEqual(user.id, user_read.id)
+
+        user_dbo = users.get_user_dbo(id=user.id, session=self.session)
+        self.assertEqual(user_dbo.password, users.generate_password('shellc', user_dbo.salt))
+
+        orgs = users.list_orgs(user_id=user_read.id, session=self.session)
+        self.assertIsInstance(orgs, users.OrganizationList)
+        self.assertEqual(len(orgs.data), 1)
+        self.assertEqual(orgs.data[0].display_name, user.display_name)
+        self.assertEqual(orgs.data[0].is_primary, True)
+        #self.assertEqual(orgs.data[0].user_id, user.id)
