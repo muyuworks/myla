4fc6de30c4493f9026d620f4ca1a970a171702fb
shellc <wangshenggong@gmail.com>
2023-12-15 17:17:40 +0800

    FEATURE: faiss group

---
+++ myla/vectorstores/faiss_group.py
@@ -0,0 +1,242 @@
+import os
+import threading
+import pickle
+import gc
+import numpy as np
+from typing import Any, Dict, List
+from ._base import Record, VectorStore
+from ._embeddings import Embeddings
+from .. import utils
+
+
+class FAISSGroupException(Exception):
+    """"""
+
+
+def _import_faiss():
+    """Import faiss package."""
+    try:
+        import faiss
+    except ImportError as exc:
+        raise ImportError(
+            "Could not import faiss python package. "
+            "Please install it with `pip install faiss-cpu` or `pip install faiss-gpu`."
+        ) from exc
+    return faiss
+
+
+class FAISSGroup(VectorStore):
+    def __init__(self, path: str, embeddings: Embeddings = None) -> None:
+        self._path = path
+        self._embeddings = embeddings
+        self._faiss = _import_faiss()
+
+        os.makedirs(name=self._path, exist_ok=True)
+
+        # lock the vs
+        self._vs_lock = threading.Lock()
+        self._col_locks = {}
+
+        self._data = {}
+        self._indexes = {}
+        self._ids = {}
+
+    def create_collection(self, collection: str, schema: Dict[str, type] = None, mode="create"):
+        """Create a collection.
+
+        :type mode:str
+        :param mode: creation mode, create: raise FAISSGroupException if exists; overwrite: drop it if exists
+        """
+        col_path = os.path.join(self._path, collection)
+
+        if mode == 'create' and os.path.exists(col_path):
+            raise FAISSGroupException(f"Collection exists: {collection}")
+
+        if mode == 'overwrite' and os.path.exists(col_path):
+            self.drop(collection=collection)
+
+        if not os.path.exists(col_path):
+            os.mkdir(col_path)
+
+    def add(
+            self,
+            collection: str,
+            records: List[Record],
+            embeddings_columns: List[str] = None,
+            vectors: List[List[float]] = None,
+            group_by: str = None
+        ):
+        """Add records to the collection."""
+        self._check_collection_exists(collection=collection)
+
+        if records is None:
+            raise ValueError(f"Invalid records: None")
+
+        if not os.path.exists(os.path.join(self._path, collection)):
+            raise FAISSGroupException(f"Collection not exists: {collection}")
+
+        groups = self._group_records(records=records, group_by=group_by)
+
+        if vectors:
+            if len(vectors) != len(records):
+                raise ValueError("The length of records must be the same as the length of vecotors.")
+        else:
+            text_to_embed = []
+            for r in records:
+                text_to_embed.append(Record.values_to_text(r, props=embeddings_columns))
+            vectors = self._embeddings.embed_batch(texts=text_to_embed)
+
+        with self._get_collection_lock(collection=collection):
+            data, indexes, ids = self._load(collection=collection)
+
+            idx_start = len(data)
+
+            # add records to data
+            for r in records:
+                data.append(r)
+
+            for gid, g_records_ids in groups.items():
+                index = indexes.get(gid)
+                id_map = ids.get(gid)
+
+                if not index:
+                    index = self._faiss.IndexFlatL2(len(vectors[0]))
+                    id_map = []
+                    indexes[gid] = index
+                    ids[gid] = id_map
+
+                g_vectors = []
+
+                for i in g_records_ids:
+                    g_vectors.append(vectors[i])
+                    id = idx_start + i
+                    id_map.append(id)
+
+                g_vectors_npa = np.array(g_vectors, dtype=np.float32)
+                self._faiss.normalize_L2(g_vectors_npa)
+                index.add(g_vectors_npa)
+
+            # save data
+            self._save_data(collection=collection, data=data)
+            gid_to_saved = groups.keys()
+            for gid in gid_to_saved:
+                self._save_group(collection=collection, gid=gid, index=indexes[gid], ids=ids[gid])
+
+    def _group_id(self, v):
+        if v is None:
+            v = ""
+        return utils.sha256(v.encode()).hex()
+
+    def _group_records(self, records: List[Record], group_by: str):
+        groups = {}
+
+        if not group_by:
+            groups[self._group_id("")] = range(len(records))
+        else:
+            for i in range(len(records)):
+                gid = records[i].get(group_by)
+                if not gid:
+                    gid = ""
+
+                gid = self._group_id(gid)
+                g = groups.get(gid)
+                if not g:
+                    g = []
+                    groups[gid] = g
+                g.append(i)
+
+        return groups
+
+    def _save_data(self, collection, data):
+        fname = os.path.join(self._path, collection, "data.pkl")
+        with open(fname, 'wb') as f:
+            pickle.dump(data, f)
+
+    def _save_group(self, collection, gid, index, ids):
+        index_fname = os.path.join(self._path, collection, f"{gid}.index")
+        self._faiss.write_index(index, index_fname)
+        ids_fname = os.path.join(self._path, collection, f"{gid}.ids")
+        with open(ids_fname, 'wb') as f:
+            pickle.dump(ids, f)
+
+    def delete(self, collection: str, query: str):
+        return super().delete(collection, query)
+
+    def search(
+            self,
+            collection: str = None,
+            query: str = None,
+            vector: List = None,
+            filter: Any = None,
+            limit: int = 20,
+            columns: List[str] | None = None,
+            with_vector: bool = False,
+            with_distance: bool = False,
+            **kwarg
+        ) -> List[Record] | None:
+        self._check_collection_exists(collection=collection)
+
+        data, indexes, ids = self._load(collection=collection)
+
+        return super().search(collection, query, vector, filter, limit, columns, with_vector, with_distance, **kwargs)
+
+    def drop(self, collection: str):
+        """"""
+
+    def _check_collection_exists(self, collection):
+        if collection is None:
+            raise ValueError(f"Invalid collection name: {collection}")
+        if not os.path.exists(os.path.join(self._path, collection)):
+            raise FAISSGroupException(f"Collection not exists: {collection}")
+
+    def _load(self, collection: str):
+        """Load collection data."""
+
+        with self._vs_lock:
+            data = self._data.get(collection)
+            indexes = self._indexes.get(collection)
+            ids = self._ids.get(collection)
+
+            if not data:
+                data = []
+                indexes = {}
+                ids = {}
+
+                path = os.path.join(self._path, collection)
+                for fname in os.listdir(path=path):
+                    if fname == "data.pkl":
+                        with open(os.path.join(path, fname), "rb") as f:
+                            data = pickle.load(f)
+                    if fname.endswith(".index"):
+                        gid = fname.replace(".index", "")
+                        idx = self._faiss.read_index(os.path.join(path, fname))
+                        indexes[gid] = idx
+
+                        id_map = []
+                        with open(os.path.join(path, f"{gid}.ids"), "rb") as f:
+                            id_map = pickle.load(f)
+                        ids[gid] = id_map
+
+                self._data[collection] = data
+                self._indexes[collection] = indexes
+                self._ids[collection] = ids
+
+        return data, indexes, ids
+
+    def _unload(self, collection: str):
+        """Unload collection."""
+        with self._vs_lock:
+            if collection in self._data:
+                del self._data[collection]
+                del self._indexes[collection]
+                del self._ids[collection]
+
+                return gc.collect()
+
+    def _get_collection_lock(self, collection):
+        with self._vs_lock:
+            lock = self._col_locks.get(collection)
+            if not lock:
+                lock = threading.Lock()
+                self._col_locks[collection] = lock
+            return lock
---
+++ tests/myla/vectorstores/faiss_group_test.py
@@ -0,0 +1,100 @@
+import os
+import shutil
+import unittest
+from myla.vectorstores.faiss_group import FAISSGroup
+from myla.utils import random_id, sha256
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+class FAISSGroupTests(unittest.TestCase):
+    def setUp(self) -> None:
+        self._vectors = [
+            [0, 0],
+            [1, 1],
+            [2, 2],
+            [3, 3],
+            [4, 4]
+        ]
+
+        self._records = [
+            {
+                'id': 0,
+                'gid': 'g0',
+            },
+            {
+                'id': 1,
+                'gid': 'g0',
+            },
+            {
+                'id': 2,
+                'gid': 'g2',
+            },
+            {
+                'id': 3,
+                'gid': 'g3',
+            },
+            {
+                'id': 4,
+            }
+        ]
+
+        self._data = os.path.abspath(os.path.join(here, os.pardir, os.pardir, 'data', random_id()))
+
+    def tearDown(self) -> None:
+        if os.path.exists(self._data):
+            shutil.rmtree(self._data)
+            pass
+
+    def test_create_collection(self):
+        vs = FAISSGroup(path=self._data)
+        vs.create_collection(collection='col')
+
+    def test_add(self):
+        vs = FAISSGroup(path=self._data)
+        vs.create_collection(collection='col')
+
+        vs.add(collection='col', records=self._records, vectors=self._vectors, group_by='gid')
+        self.assertIsNotNone(vs._data.get('col'))
+        self.assertEqual(vs._data.get('col'), self._records)
+        self.assertIsNotNone(vs._indexes.get('col'))
+        self.assertIsNotNone(vs._ids.get('col'))
+
+        self.assertEqual(len(vs._indexes.get('col')), 4)
+        self.assertEqual(len(vs._ids.get('col')), 4)
+
+        self.assertEqual(vs._indexes.get('col').keys(), vs._ids.get('col').keys())
+
+        gids = list(vs._indexes.get('col').keys())
+        gids.sort()
+        gids_1 = []
+        for r in self._records:
+            gids_1.append(sha256(r.get('gid', '').encode()).hex())
+        gids_1 = list(set(gids_1))
+        gids_1.sort()
+
+        self.assertEqual(gids, gids_1)
+
+        self.assertEqual(vs._ids.get('col')[vs._group_id("")], [4])
+        self.assertEqual(vs._ids.get('col')[vs._group_id("g0")], [0, 1])
+        self.assertEqual(vs._ids.get('col')[vs._group_id("g2")], [2])
+        self.assertEqual(vs._ids.get('col')[vs._group_id("g3")], [3])
+
+        vs.add(collection='col', records=[{'gid': 'g2'}, {'gid': 'g3', 'id': 6}], vectors=[[5, 5], [6, 6]], group_by='gid')
+        self.assertEqual(vs._ids.get('col')[vs._group_id("g2")], [2, 5])
+        self.assertEqual(vs._ids.get('col')[vs._group_id("g3")], [3, 6])
+
+        self.assertEqual(vs._data.get('col')[6]['id'], 6)
+
+    def test_load(self):
+        vs = FAISSGroup(path=self._data)
+        vs.create_collection(collection='col')
+
+        vs.add(collection='col', records=self._records, vectors=self._vectors, group_by='gid')
+
+        vs._unload(collection='col')
+        self.assertIsNone(vs._data.get('col'))
+
+        vs._load(collection='col')
+        self.assertIsNotNone(vs._data.get('col'))
+        self.assertEqual(vs._data.get('col'), self._records)
