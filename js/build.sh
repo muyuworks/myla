926142b0a725390c72823bc1d22c0dfb1352f235
shellc <wangshenggong@gmail.com>
2023-12-19 15:51:09 +0800

    FIX: js build script

--- js/build.sh
+++ js/build.sh
@@ -1,5 +1,5 @@
 npm run build
 
-mkdir -p ../myla/webui/static/aify
+mkdir -p ../myla/webui/statics/aify
 cp build/static/js/*.js ../myla/webui/statics/aify/aify.js
 cp build/static/css/*.css ../myla/webui/statics/aify/aify.css
