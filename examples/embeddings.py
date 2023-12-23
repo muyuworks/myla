902d5378cfe74cf43abfc6418b2132bdf6ef7b75
shellc <wangshenggong@gmail.com>
2023-12-23 15:55:46 +0800

    TEST: normlize_L2

--- examples/embeddings.py
+++ examples/embeddings.py
@@ -1,6 +1,15 @@
 import asyncio
+import numpy as np
+import faiss
 from myla.vectorstores.sentence_transformers_embeddings import SentenceTransformerEmbeddings
 
 embeddings = SentenceTransformerEmbeddings(model_name="/Users/shellc/Downloads/bge-large-zh-v1.5")
 
-print(asyncio.run(embeddings.aembed("hello")))
+v = asyncio.run(embeddings.aembed("hello"))
+
+v = np.array([v], dtype=np.float32)
+
+faiss.normalize_L2(v)
+
+n = np.linalg.norm(v, ord=2)
+print(n)
