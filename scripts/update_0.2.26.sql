3c618d0a2cf27b057040adb9e178ab28c7291ad4
shellc <wangshenggong@gmail.com>
2024-05-17 14:37:59 +0800

    RELEASE: 0.2.26

--- scripts/update_0.2.26.sql
+++ scripts/update_0.2.26.sql
@@ -1,6 +1,8 @@
 ALTER TABLE userorglink ADD "role" VARCHAR;
 UPDATE userorglink SET role='owner';
 
+UPDATE organization SET user_id=(SELECT userorglink.user_id FROM userorglink WHERE organization.id=userorglink.org_id);
+
 UPDATE assistant SET org_id=(SELECT userorglink.org_id FROM userorglink WHERE assistant.user_id=userorglink.user_id);
 UPDATE thread SET org_id=(SELECT userorglink.org_id FROM userorglink WHERE thread.user_id=userorglink.user_id);
 UPDATE message SET org_id=(SELECT userorglink.org_id FROM userorglink WHERE message.user_id=userorglink.user_id);
