54ab227e5a64ce2de5be01d9a44fa2e201839469
shellc <wangshenggong@gmail.com>
2024-06-25 15:01:30 +0800

    FEATURE: support for xinf multi model_id

--- myla/vectorstores/xinference_embeddings.py
+++ myla/vectorstores/xinference_embeddings.py
@@ -1,3 +1,4 @@
+from random import randint
 from typing import List
 
 from xinference_client import RESTfulClient as Client
@@ -17,11 +18,21 @@ class XinferenceEmbeddings(Embeddings):
         self._instruction = instruction
 
         client = Client(self._base_url)
-        self._model = client.get_model(model_id)
+        #self._model = client.get_model(model_id)
+        model_ids = model_id.split(",")
+        self._models = []
+        for m_id in model_ids:
+            self._models.append(client.get_model(m_id))
 
     def embed_batch(self, texts: List[str], **kwargs) -> List[List[float]]:
         if self._instruction is not None:
             texts = [self._instruction + t for t in texts]
 
-        embeds = self._model.create_embedding(texts)
+        model = self._get_model()
+
+        embeds = model.create_embedding(texts)
         return [e["embedding"] for e in embeds["data"]]
+
+    def _get_model(self):
+        idx = randint(0, len(self._models) - 1)
+        return self._models[idx]
